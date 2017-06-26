c53f12812cff20c22669266ab1c376e92a3d8003
superbiji <you@example.com>
2017-06-26 12:39:57 -0400

    Reorganisation des fichiers

---
+++ scripts/platform-controller.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+stty -F /dev/serial0 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+
+( cat < /dev/serial0 ) &
+
+while true
+do
+	read input
+	echo "$input" > /dev/serial0
+done
