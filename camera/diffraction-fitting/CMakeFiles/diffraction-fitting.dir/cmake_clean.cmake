c7d05692da7c84cde1e5b25b1fc0968ff9a33264
superbiji <you@example.com>
2017-10-17 08:47:38 -0400

    Demarrage du projet de detection de diffraction

---
+++ camera/diffraction-fitting/CMakeFiles/diffraction-fitting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diffraction-fitting.dir/source/main.cpp.o"
+  "diffraction-fitting.pdb"
+  "diffraction-fitting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diffraction-fitting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
