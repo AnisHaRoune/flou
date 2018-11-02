4ce2e4388b347c86c4e5e3c337fe0317040a8607
BenjaminLapointe-Pinel <benjamin.lapointe-pinel@uqar.ca>
2018-11-02 14:41:17 -0400

    threshold + beginning of center-diffraction

--- center-finder/center-average.py
+++ center-finder/center-average.py
@@ -5,16 +5,11 @@ import cv2
 image = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
 
 if len(sys.argv) >= 3:
-    t = int(sys.argv[2])
+    t = float(sys.argv[2])
 else:
     t = 0
 
-if (len(sys.argv) >= 4):
-    debug = sys.argv[3] == "debug"
-else:
-    debug = False
-
-X = numpy.array([0, 0])
+X = numpy.array([0, 0], dtype=numpy.float)
 Y = 0
 for (x, y), value in numpy.ndenumerate(image):
     pixel = max(image[x, y] - t, 0)
@@ -24,7 +19,7 @@ for (x, y), value in numpy.ndenumerate(image):
 center = X/Y
 print(center)
 
-if debug:
+if sys.argv[3] == "debug":
     x = int(center[0])
     y = int(center[1])
     image[x, y] = 0
