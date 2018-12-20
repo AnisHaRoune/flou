305ef3a50ab407345b0f546e0665dc65a5fa9aa5
BenjaminLapointe-Pinel <benjamin.lapointe-pinel@uqar.ca>
2018-12-20 15:20:08 -0500

    better naming convention

--- scripts/capture/capture.ps1
+++ scripts/capture/capture.ps1
@@ -9,7 +9,6 @@
     [Parameter(Mandatory=$true)]
     [string] $unity,
     [int] $number_of_captures_by_step = 1,
-    [string] $capture_name = "capture",
     [int] $max_error_count = 10,
     [string] $camera_name = "STC-MBS231U3V(16H6167)"
 )
@@ -30,14 +29,15 @@ else
         Write-Host "$COM open."
         Write-Host "Begin platform control."
 
-        for($index, $i = 0; $i -lt $number_of_captures; $i++)
+        for($i = 0; $i -lt $number_of_captures; $i++)
         {
-            for($j = 0; $j -lt $number_of_captures_by_step; $j++, $index++)
+            for($j = 0; $j -lt $number_of_captures_by_step; $j++)
             {
                 Write-Host "ffmpeg capture..."
-                $leading_index = $index.ToString().PadLeft(8, "0")
                 $distance_index = $distance * $i
-                $filepath = "captures\$leading_index-$capture_name-$distance_index$unity-$j.png"
+				$distance_name = $distance_index.ToString().PadLeft(8, "0")
+				$capture_index = $j.ToString().PadLeft(8, "0")
+                $filepath = "captures\$distance_name-$unity-$capture_index.png"
                 $result = .\ffmpeg-3.4.2-win32-static\bin\ffmpeg.exe -loglevel error -f dshow -rtbufsize 128M -i video="$camera_name" -vf vflip -vframes 1 "$filepath" -y 2>&1
                 $error = $result -match "error"
                 if ($error)
